3ac39007e82c959cc21cd77126e883ecf862b078
0racularGh0st <nigel4tariang@gmail.com>
2020-11-01 16:20:48 +0530

    add logout function

--- src/components/User.vue
+++ src/components/User.vue
@@ -1,41 +1,63 @@
 <template>
-    <div>
-        <div v-if="dataReady">Spotify User: {{user.display_name}}</div>
-    </div>
+  <div>
+    <div v-if="dataReady">Spotify User: {{ user.display_name }}</div>
+    <button @click="signout()">Logout</button>
+  </div>
 </template>
 <script>
-import { getUserInfo} from '../spotify';
-import { catchErrors } from '../utils';
+import { getUserInfo, logout } from "../spotify";
+import { catchErrors } from "../utils";
 export default {
-    data(){
-        return{
-            user: null,
-            followedArtists: null, 
-            playlists: null, 
-            topArtists: null, 
-            topTracks: null,
-            dataReady: false
-        }
-    },
-    created(){
-        catchErrors(this.getData());
-    },
-    methods:{
-        getData: async function() {
-    const { user, followedArtists, playlists, topArtists, topTracks } = await getUserInfo();
-    await this.assignData(user, followedArtists, playlists, topArtists, topTracks);
-    console.log(user, followedArtists, playlists, topArtists, topTracks);
-    console.log("The User is:", this.user);
+  data() {
+    return {
+      user: null,
+      followedArtists: null,
+      playlists: null,
+      topArtists: null,
+      topTracks: null,
+      dataReady: false,
+    };
   },
-  assignData: async function(user, followedArtists, playlists, topArtists, topTracks){
-            this.user = user;
-            this.followedArtists = followedArtists;
-            this.playlists = playlists;
-            this.topArtists = topArtists;
-            this.topTracks = topTracks;
-            this.dataReady = true;
-            return;
-  }
+  created() {
+    catchErrors(this.getData());
+  },
+  methods: {
+    getData: async function () {
+      const {
+        user,
+        followedArtists,
+        playlists,
+        topArtists,
+        topTracks,
+      } = await getUserInfo();
+      await this.assignData(
+        user,
+        followedArtists,
+        playlists,
+        topArtists,
+        topTracks
+      );
+      console.log(user, followedArtists, playlists, topArtists, topTracks);
+      console.log("The User is:", this.user);
+    },
+    assignData: async function (
+      user,
+      followedArtists,
+      playlists,
+      topArtists,
+      topTracks
+    ) {
+      this.user = user;
+      this.followedArtists = followedArtists;
+      this.playlists = playlists;
+      this.topArtists = topArtists;
+      this.topTracks = topTracks;
+      this.dataReady = true;
+      return;
+    },
+    signout: function(){
+        logout();
     }
-}
+  }
+};
 </script>
--- src/spotify/index.js
+++ src/spotify/index.js
@@ -65,7 +65,7 @@ export const logout = () => {
   window.localStorage.removeItem('spotify_token_timestamp');
   window.localStorage.removeItem('spotify_access_token');
   window.localStorage.removeItem('spotify_refresh_token');
-  window.location.reload();
+  window.location="/";
 };
 
 // API CALLS ***************************************************************************************
