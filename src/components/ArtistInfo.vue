93a00e097fba254978d275f2420a91ed7c009693
0racularGh0st <nigel4tariang@gmail.com>
2020-11-08 14:07:21 +0530

    update artist info page

--- src/components/ArtistInfo.vue
+++ src/components/ArtistInfo.vue
@@ -4,7 +4,41 @@
       <Loader />
     </div>
     <div class="content-container" v-if="dataReady">
-      </div>
+       <div class="artist-art-name-container">
+            <img :src="artistInfo.images[1].url" height="250" width="250" alt="artist-art" class="artist-art"/>
+            <div>
+                <h1>{{artistInfo.name}}</h1>
+            </div>
+       </div>
+       <div class="artist-details">
+           <div class="artist-detail-item">
+                <div class="detail-name">
+                    FOLLOWERS
+                </div>
+               <div class="detail-value">
+                   {{artistInfo.followers.total}}
+               </div>
+           </div>
+           <div class="artist-detail-item">
+                <div class="detail-name">
+                    GENRES
+                </div>
+               <div class="detail-value">
+                   <div v-for="(item,index) in artistInfo.genres" v-bind:key="index">
+                       {{item}}
+                   </div>
+               </div>
+           </div>
+           <div class="artist-detail-item">
+                <div class="detail-name">
+                    POPULARITY
+                </div>
+               <div class="detail-value">
+                    {{artistInfo.popularity}}%
+               </div>
+           </div>
+       </div>
+    </div>
   </div>
 </template>
 <script>
@@ -26,7 +60,7 @@ export default {
     getArtistData: async function () {
       let artistInfoRes = await getArtist(this.artistId);
       this.artistInfo = artistInfoRes.data;
-      //this.dataReady = true;
+      this.dataReady = true;
       console.log("Res->", this.artistInfo);
     }
   },
@@ -36,6 +70,7 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
+h1,
 h2,
 h3,
 p {
@@ -51,7 +86,33 @@ p {
   width: 100%;
   background: var(--bg-color);
 }
-
-
-
+.artist-art{
+    border-radius: 50%;
+    margin-bottom: 1rem;
+}
+.artist-art-name-container{
+   display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+.artist-details{
+    margin-top: 2rem;
+    display: flex;
+    justify-content: space-around;
+}
+.detail-name{
+    color: var(--main-white);
+    opacity: 0.8;
+    font-size: 14px;
+    margin-bottom: 0.5rem;
+}
+.detail-value{
+    color: var(--alt-green);
+    font-size: 16px;
+    font-weight: 600;
+}
+.artist-detail-item{
+    display: flex;
+    flex-direction: column;
+}
 </style>
